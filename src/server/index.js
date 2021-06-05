07a28c5b102e1fa1c0f755e74de78dbd07fe1da9
Sameer Mitra <49610563+sameer-mitra@users.noreply.github.com>
2021-06-05 17:17:06 +1000

    SM 20210605 More changes to config files.

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -7,6 +10,7 @@ const app = express()
 app.use(express.static('dist'))
 
 console.log(__dirname)
+console.log(`Your API key is ${process.env.API_KEY}`);
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
